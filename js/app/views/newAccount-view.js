6477dfb049d41d160e3b7f8d53865de3d9fd9ed2
DiogoGitsGood <diogo.martins@academiadecodigo.org>
2023-04-28 05:58:47 +0100

    add newAccount
    newAccounts added sucessfully.

--- js/app/views/newAccount-view.js
+++ js/app/views/newAccount-view.js
@@ -1,4 +1,4 @@
-import newAccountController from "../controllers/newAccount-controller.js";
+
 const externals = {};
 
 function renderNewAccountForm(app) {
@@ -20,28 +20,41 @@ function renderNewAccountForm(app) {
   const passwordInput = $('<input>').attr('id', 'password').attr('type', 'password').attr('placeholder', 'Password').attr('required', true);
   const emailInput = $('<input>').attr('id', 'email').attr('type', 'email').attr('placeholder', 'Email').attr('required', true);
 
+
   const humansDescription = "Humans are a proud race of warriors who value strength and honor above all else. They are known for their exceptional swordsmanship and their powerful war cry.";
   const orcsDescription = "Orcs are a fierce and brutal race that lives for battle. They are known for their ferocity in combat and their ability to take on much larger opponents.";
+  const elvesDescription ="Elves are a graceful and wise race that values harmony with nature. They are known for their archery and their ability to move silently through the forest.";
+  
+  
   const tribeSelection = $('<div>').addClass('tribe-selection').attr('id', 'tribe').append(
-    $('<div>').addClass('tribe-description').text('Choose your tribe:'),
-    $('<label>').append(
-      $('<input>').attr('type', 'radio').attr('name', 'tribe').val('humans').prop('checked', true),
-      $('<span>').text('Humans'),
-      $('<div>').addClass('tribe-info').text(humansDescription),
+    $('<div>').addClass('tribe-description').text('Choose your tribe:').css({'margin-top':'40px'}),
+    $('<div>').addClass('tribe-options').append(
+      $('<div>').addClass('tribe-option').append(
+        $('<label>').append(
+          $('<input>').attr('type', 'radio').attr('name', 'tribe').val('humans').prop('checked', true),
+          $('<div>').addClass('tribe-image').append($('<img>').attr('src', 'img/human.jpg')).css({'max-width':'100px', 'height': 'auto','margin-top':'20px'})),
+        $('<div>').addClass('tribe-info').text(humansDescription)
+      )
     ),
-    $('<label>').append(
-      $('<input>').attr('type', 'radio').attr('name', 'tribe').val('orcs'),
-      $('<span>').text('Orcs'),
-      $('<div>').addClass('tribe-info').text(orcsDescription),
+    $('<div>').addClass('tribe-options').append(
+      $('<div>').addClass('tribe-option').append(
+        $('<label>').append(
+          $('<input>').attr('type', 'radio').attr('name', 'tribe').val('orcs'),
+          $('<div>').addClass('tribe-image').append($('<img>').attr('src', 'img/orc.jpg'))),
+        $('<div>').addClass('tribe-info').text(orcsDescription)
+      )
     ),
-    $('<label>').append(
-      $('<input>').attr('type', 'radio').attr('name', 'tribe').val('elves'),
-      $('<span>').text('Elves'),
-      $('<div>').addClass('tribe-info').text('Elves are a graceful and wise race that values harmony with nature. They are known for their archery and their ability to move silently through the forest.'),
-    ),
-  );
+    $('<div>').addClass('tribe-options').append(
+      $('<div>').addClass('tribe-option').append(
+        $('<label>').append(
+          $('<input>').attr('type', 'radio').attr('name', 'tribe').val('elves'),
+          $('<div>').addClass('tribe-image').append($('<img>').attr('src', 'img/elf.jpg'))),
+        $('<div>').addClass('tribe-info').text(elvesDescription))
+      )
+    )
+  
 
-  const submitButton = $('<button>').attr('type', 'submit').text('Create Account');
+  const submitButton = $('<button>').attr('type', 'submit').text('Create Account').css({'margin-top':'40px'});
 
   form.append(usernameInput);
   form.append(passwordInput);
@@ -50,10 +63,12 @@ function renderNewAccountForm(app) {
   form.append(submitButton);
 
   app.append(form);
+
   const backButton = $('<button>').text('Back to Home');
   backButton.on('click', function() {
     window.location.hash = '#/';
   });
+
   app.append(backButton);
 }
 
